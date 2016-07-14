cd2e686bd56d5eb5d2d8366ea73db3af607c8552
andrewjpage <andrewjpage@gmail.com>
2016-07-14 13:32:06 +0100

    allow full VFDB to be downloaded

--- ariba/ref_genes_getter.py
+++ ariba/ref_genes_getter.py
@@ -13,7 +13,7 @@ from ariba import common, card_record, vfdb_parser
 
 class RefGenesGetter:
     def __init__(self, ref_db, genetic_code=11):
-        allowed_ref_dbs = {'card', 'argannot', 'resfinder','vfdb'}
+        allowed_ref_dbs = {'card', 'argannot', 'resfinder','vfdb_full','vfdb_core'}
         if ref_db not in allowed_ref_dbs:
             raise Error('Error in RefGenesGetter. ref_db must be one of: ' + str(allowed_ref_dbs) + ', but I got "' + ref_db)
         self.ref_db=ref_db
@@ -237,26 +237,32 @@ class RefGenesGetter:
         print('If you use this downloaded data, please cite:')
         print('"ARG-ANNOT, a new bioinformatic tool to discover antibiotic resistance genes in bacterial genomes",\nGupta et al 2014, PMID: 24145532\n')
 
-    def _get_from_vfdb(self, outprefix):
+    def _get_from_vfdb_core(self, outprefix):
+        self._get_from_vfdb_common('VFDB_setA_nt.fas.gz','core')
+		
+    def _get_from_vfdb_full(self, outprefix):
+        self._get_from_vfdb_common('VFDB_setB_nt.fas.gz','full')
+	
+    def _get_from_vfdb_common(self,filename,info_text):
         outprefix = os.path.abspath(outprefix)
         tmpdir = outprefix + '.tmp.download'
         current_dir = os.getcwd()
-
+        
         try:
             os.mkdir(tmpdir)
         except:
             raise Error('Error mkdir ' + tmpdir)
-
-        zipfile = os.path.join(tmpdir, 'VFDB_setA_nt.fas.gz')
-        self._download_file('http://www.mgc.ac.cn/VFs/Down/VFDB_setA_nt.fas.gz', zipfile)
+        
+        zipfile = os.path.join(tmpdir, filename)
+        self._download_file('http://www.mgc.ac.cn/VFs/Down/'+filename, zipfile)
         vparser = vfdb_parser.VfdbParser(zipfile, outprefix)
         vparser.run()
         shutil.rmtree(tmpdir)
         print('Extracted files.')
         final_fasta = outprefix + '.presence_absence.fa'
         final_tsv = outprefix + '.metadata.tsv'
-
-        print('Extracted core DNA sequence dataset and metadata. Final files are:', final_fasta, final_tsv, sep='\n\t', end='\n\n')
+        
+        print('Extracted '+info_text+' DNA sequence dataset and metadata. Final files are:', final_fasta, final_tsv, sep='\n\t', end='\n\n')
         print('You can use it with ARIBA like this:')
         print('ariba prepareref --ref_prefix', outprefix, 'output_directory\n')
         print('If you use this downloaded data, please cite:')
